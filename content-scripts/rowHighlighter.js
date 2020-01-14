47f6433c62a784626a3bd8b521594d7067deb3b6
Dave Trost <davetrost@gmail.com>
2020-01-14 13:04:12 -0800

    refactor by dave

--- content-scripts/rowHighlighter.js
+++ content-scripts/rowHighlighter.js
@@ -1,40 +1,35 @@
-(function() {
-  console.log('highlight-active-row: rowHighlighter Injected!');
+const defaultStyle = '';
+const highlightStyle = 'font-weight: bold; background-color: yellow; outline: thin solid';
 
-  // Attach focusin/focusout listeners to the document;
-  document.addEventListener('focusin', onFocusIn, true);
-  document.addEventListener('focusout', onFocusOut, true);
+// Attach focusin/focusout listeners to the document;
+document.addEventListener('focusin', onFocusIn, true);
+document.addEventListener('focusout', () => onFocusChange(event, defaultStyle), true);
 
-  function onFocusIn({ target }) {
-    setStyle(
-      getRowNode(target),
-      'font-weight: bold; background-color: yellow; outline: thin solid'
-    );
-  }
+const onFocusIn = ({ target }) => findRowAndSetStyle(target, highlightStyle);
+const onFocusOut = ({ target }) => findRowAndSetStyle(target, defaultStyle);
 
-  function onFocusOut({ target }) {
-    setStyle(getRowNode(target), '');
-  }
+function findRowAndSetStyle(target, styleString) {
+  setStyle(getRowNode(target), styleString);
+}
 
-  function getRowNode(el) {
-    const MAX_DEPTH = 5;
-    let rowNode;
-    let curEl = el;
-    for (let i = 0; i < MAX_DEPTH; i++) {
-      if (curEl && curEl.tagName === 'TR') {
-        rowNode = curEl;
-        break;
-      }
-      curEl && (curEl = curEl.parentNode);
-      if (MAX_DEPTH === i + 1)
-        console.log(
-          'highlight-active-row: Unable to find Row Element. Increase MAX_DEPTH'
-        );
+function getRowNode(el) {
+  const MAX_DEPTH = 5;
+  let curEl = el;
+  for (let i = 0; i < MAX_DEPTH; i++) {
+    if (!curEl) return null;
+    if (curEl.tagName === 'TR') return curEl;
+
+    curEl = curEl.parentNode;
+
+    if (i + 1 >= MAX_DEPTH) {
+      console.log('highlight-active-row: Unable to find Row Element. Increase MAX_DEPTH');
     }
-    return rowNode;
   }
+  return null;
+}
 
-  function setStyle(el, style) {
-    el && (el.style.cssText = style);
+function setStyle(el, style) {
+  if (el) {
+    el.style.cssText = style;
   }
-})();
+}
